7bdccb8afc3d35ba474053eeaf9f15deb1c39221
RaghavVashisht <raghav.vashisht91@gmail.com>
2016-02-15 12:37:31 -0500

    Fixed "Like" integration and some UI changes

---
+++ BlogApp4/Migrations/201602150350511_Userna.cs
@@ -0,0 +1,16 @@
+namespace BlogApp4.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Userna : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
