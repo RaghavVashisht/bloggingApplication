39dde5124e93b24bb1c03572c7548285cc5e78a8
RaghavVashisht <raghav.vashisht91@gmail.com>
2016-02-24 23:13:53 -0500

    Added Migration files

---
+++ BlogApp4/Migrations/201602150348264_UserN.cs
@@ -0,0 +1,16 @@
+namespace BlogApp4.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UserN : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
